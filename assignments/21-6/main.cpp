d88c7fd93d8c250c434ad93a6d74cf9a01f3e821
steph1111 <stephanielh1111@gmail.com>
2024-04-05 02:08:08 -0700

    Main basic I/O functonality

--- assignments/21-6/main.cpp
+++ assignments/21-6/main.cpp
@@ -11,8 +11,17 @@
 #include <iostream>
 #include "maze.h"
 
-int main() {
-    Maze maze(10);
-    std::cout << maze;
+int main(int argc, char** argv) {
+    if (argc > 1) {
+        int n = std::atoi(argv[1]);
+        if (n < 3) {
+            std::cerr << "Error: (Argument 1) Maze length must be 3 or greater.\n";
+        } else {
+            Maze maze(n);
+            std::cout << maze;
+        }
+    } else {
+        std::cerr << "Error: Must provide an argument specifying the maze length.\n";
+    }
     return 0;
 }
