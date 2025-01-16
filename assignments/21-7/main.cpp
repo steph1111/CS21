c5a56d6893b85b4bb875bca7aac7a71d72754ed0
steph1111 <stephanielh1111@gmail.com>
2025-01-16 15:08:03 -0800

    Filled in choices for input (UNTESTED)

--- assignments/21-7/main.cpp
+++ assignments/21-7/main.cpp
@@ -4,23 +4,27 @@
  * Email:   stephanielh1111@gmail.com
  * @date    05/02/24
  * CS21:    Assignment#7
- * Status:  Working/Tested
+ * Status:  Untested
  *
  * Demonstrates functionality of the `BinarySearchTree` class.
 */
 
 #include <iostream>
 #include <unordered_map>
+#include <sstream>
 #include "binary_search_tree.h"
 
-std::pair<int, std::string> split_data(std::string str) {
-    int i = str.find(' ');
-    return std::make_pair(std::stoi(str.substr(0, i)), 
-        str.substr(i + 1, str.size() - i));
+void print_vector(const std::vector<int>& keys) {
+    for (unsigned i = 0; i < keys.size(); i++) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
 }
 
 int main() {
-    std::string input, choice;
+    BinarySearchTree bst;
+    std::string input, action;
+    int num;
     std::pair<std::string, std::string> split_input;
     enum {
         search, min, max, insert, successor, predecessor, inorder, postorder, preorder, remove, comment
@@ -37,43 +41,104 @@ int main() {
         {"postorder",   postorder},
         {"preorder",    preorder},
         {"delete",      remove},
-        {"#",           comment},
+        {"#",           comment}
     };
 
-    try { // Try block to avoid memory leaks for invalid input
+    try {   // Try block to avoid memory leaks for invalid input
         while(std::getline(std::cin, input)){
-            split_input = split_data(input);
-            choice = split_input.first;
-            auto data = choice_map.find(choice);
+            std::stringstream ss(input);    // Create a new string stream of the input
+            ss >> action;   // Extract first token from string stream
+
+            // Check if desired action is in the map of valid choices
+            auto data = choice_map.find(action);
             if (data != choice_map.end()) {
                 switch ((int) data->second) {
-                    case search:
-                        break;
-                    case min:
-                        break;
-                    case max:
-                        break;
-                    case insert:
-                        break;
-                    case successor:
-                        break;
-                    case predecessor:
-                        break;
-                    case inorder:
-                        break;
-                    case postorder:
-                        break;
-                    case preorder:
-                        break;
-                    case remove:
-                        break;
-                    case comment:
-                        break;
+
+                case search:
+                    ss >> num;
+                    std::cout << num << (bst.search(num) ? " " : " not ") << "found.\n";
+                    break;
+
+                case min: {
+                    int* minimum = bst.min();
+                    if (minimum) {
+                        std::cout << "min is " << minimum << ".\n";
+                    } else {
+                        std::cout << "tree empty.\n";
+                    }
+                    delete minimum;
+                    break;
                 }
+
+                case max: {
+                    int* maximum = bst.max();
+                    if (maximum) {
+                        std::cout << "max is " << maximum << ".\n";
+                    } else {
+                        std::cout << "tree empty.\n";
+                    }
+                    delete maximum;
+                    break;
+                }
+
+                case insert:
+                    ss >> num;
+                    bst.insert(num);
+                    std::cout << "inserted " << num << ".\n";
+                    break;
+
+                case successor: {
+                    ss << num;
+                    int* inherit = bst.successor(num);
+                    if (inherit) {
+                        std::cout << num << "successor is " << inherit << ".\n";
+                    } else {
+                        std::cout << "key not in tree.\n";
+                    }
+                    delete inherit;
+                    break;
+                }
+
+                case predecessor: {
+                    ss << num;
+                    int* inherit = bst.predecessor(num);
+                    if (inherit) {
+                        std::cout << num << "predecessor is " << inherit << ".\n";
+                    } else {
+                        std::cout << "key not in tree.\n";
+                    }
+                    delete inherit;
+                    break;
+                }
+
+                case inorder:
+                    std::cout << "inorder traversal: ";
+                    print_vector(bst.inorder());
+                    break;
+
+                case postorder:
+                    std::cout << "postorder traversal: ";
+                    print_vector(bst.postorder());
+                    break;
+
+                case preorder:
+                    std::cout << "preorder traversal: ";
+                    print_vector(bst.preorder());
+                    break;
+
+                case remove:
+                    ss << num;
+                    if (bst.remove(num)) {
+                        std::cout << "deleted " << num << ".\n";
+                    } else {
+                        std::cout << "delete " << num << " - not found.\n";
+                    }
+                    break;
+                }
+
             } else {
-                std::cerr << "Enter a valid choice\n";
+                std::cerr << "Enter a valid choice.\n";
             }
-            
         }
     } catch (std::invalid_argument const&){
         std::cerr << "Invalid input. Exiting program.\n";
